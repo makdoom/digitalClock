0dd656aeb59b7884b7428857474c18056e0ab9c5
Makdoom Shaikh <makshaikh99@gmail.com>
2020-06-14 15:59:55 +0530

    Updates

---
+++ precache-manifest.0ab377ec7b5a73c0b55e2754a25db9f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6872d97358589a85be50ee38dd51c958",
+    "url": "/digitalClock/index.html"
+  },
+  {
+    "revision": "32db4d681e2849b37da0",
+    "url": "/digitalClock/static/css/main.db02f0bb.chunk.css"
+  },
+  {
+    "revision": "1141128061db63cffdeb",
+    "url": "/digitalClock/static/js/2.7b7fd549.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/digitalClock/static/js/2.7b7fd549.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32db4d681e2849b37da0",
+    "url": "/digitalClock/static/js/main.61076913.chunk.js"
+  },
+  {
+    "revision": "f459cbfbc09d21afabaa",
+    "url": "/digitalClock/static/js/runtime-main.7b28bca6.js"
+  }
+]);
